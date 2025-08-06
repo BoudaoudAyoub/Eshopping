60dae12debd910772968bb0fe22dd38dd1f47195
Ayoub Boudaoud <ayoub.boudaoud@terabyte-software.com>
2025-08-06 12:20:18 +0100

    fix the url path

--- E-Shopping/E-Shopping-Frontend/src/app/core/services/OrderService.ts
+++ E-Shopping/E-Shopping-Frontend/src/app/core/services/OrderService.ts
@@ -3,25 +3,25 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { OrderItemReq, OrderModel } from '../interfaces/OrderInterface';
 import { environment } from '../../../environments/environment';
-
+ 
 @Injectable({ providedIn: 'root' })
 export class OrderService {
   constructor(private http: HttpClient) {}
-
-  private baseUrl = `${environment}'/api/orders'`;
-
+ 
+  private baseUrl = `${environment.apiUrl}/orders`;
+ 
   CreateOrder(orderItems: OrderItemReq[]): Observable<any> {
     return this.http.post(this.baseUrl, orderItems);
   }
-
+ 
   getAllOrders(): Observable<OrderModel[]> {
     return this.http.get<OrderModel[]>(this.baseUrl);
   }
-
+ 
   cancelOrder(orderId: string): Observable<any> {
     return this.http.put(`${this.baseUrl}/${orderId}/cancel`,null);
   }
-
+ 
   deleteOrder(orderId: string): Observable<any> {
     return this.http.delete(`${this.baseUrl}/${orderId}`);
   }
--- E-Shopping/E-Shopping-Frontend/src/app/core/services/ProductService.ts
+++ E-Shopping/E-Shopping-Frontend/src/app/core/services/ProductService.ts
@@ -3,13 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../interfaces/ProductInterface';
 import { environment } from '../../../environments/environment';
-
+ 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
-
+ 
   constructor(private http: HttpClient) {}
-
+ 
   getAllProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(`${environment}'/api/products'`);
+    return this.http.get<Product[]>(`${environment.apiUrl}/products`);
   }
 }
